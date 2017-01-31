b607d1de32cc6faa3943f1527b086a2c87c7591d
Ashley Coleman <ashley78@ksu.edu>
2017-01-31 14:29:22 -0600

    Code from class

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var http = require('http');
 var fs = require('fs');
 var port = 3000;
 
+var stylesheet = fs.readFileSync('gallery.css');
+
+var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg'];
+
 function serveImage(filename, req, res){
   fs.readFile('images/' + filename, (err, body) => {
     if(err){
@@ -27,6 +31,24 @@ function serveImage(filename, req, res){
 var server = http.createServer((req, res) => {
 
   switch (req.url) {
+    case '/gallery':
+      var gHtml = imageNames.map((name) => {
+        return '<img src="' + name + '" alt="a fishing ace at work">'
+      }).join();
+
+      var html =  '<!doctype html>';
+          html += '<head>';
+          html += ' <title>Dynamic Page</title>';
+          html += ' <link href="gallery.css" rel="stylesheet" type="text/css">';
+          html += '</head>';
+          html += '<body>';
+          html += '<h1>Gallery</h1>'
+          html += gHtml;
+          html += '</body>';
+
+      res.setHeader('Content-Type', 'text/html');
+      res.end(html);
+      break;
     case '/chess':
     case '/chess/':
     case '/chess.jpg':
@@ -38,6 +60,16 @@ var server = http.createServer((req, res) => {
     case '/fern.jpg':
     case '/fern.jpg/':
       serveImage('fern.jpg', req, res);
+      break;
+    case '/ace.jpg':
+      serveImage('ace.jpg', req, res);
+      break;
+    case '/bubble.jpg':
+      serveImage('bubble.jpg', req, res);
+      break;
+    case '/gallery.css':
+      res.setHeader('Content-Type', 'text/css');
+      res.end(stylesheet);
       break;
     default:
       res.statusCode = 404;
