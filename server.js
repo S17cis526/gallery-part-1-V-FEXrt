964ad524ca7a5c9b362141deaef7d38caa4e33c7
Ashley Coleman <ashley78@ksu.edu>
2017-02-07 14:23:47 -0600

    2-7

--- server.js
+++ server.js
@@ -7,9 +7,11 @@
  */
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var port = 3000;
 
 var stylesheet = fs.readFileSync('gallery.css');
+var config = JSON.parse(fs.readFileSync('config.json'));
 
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg'];
 
@@ -44,12 +46,20 @@ function serveImage(filename, req, res){
 function buildGallery(imageTags){
     var html =  '<!doctype html>';
         html += '<head>';
-        html += ' <title>Dynamic Page</title>';
+        html += ' <title>' + config.title + '</title>';
         html += ' <link href="gallery.css" rel="stylesheet" type="text/css">';
         html += '</head>';
         html += '<body>';
-        html += '<h1>Gallery</h1>'
+        html += '<h1>' + config.title + '</h1>';
+        html += '<form>';
+        html += '  <input type="text" name="title">';
+        html += '  <input type="submit" value="Change Gallery Title">';
+        html += '</form>';
         html += imageNamesToTags(imageTags).join('');
+        html += '<form method="POST" enctype="multipart/form-data">'
+        html += '  <input type="file" name="image">'
+        html += '  <input type="submit" value="Upload Image">'
+        html += '</form>'
         html += '</body>';
 
     return html
@@ -70,19 +80,61 @@ function serveGallery(req, res) {
   });
 }
 
-var server = http.createServer((req, res) => {
+function uploadImage(req, res){
+  var body='';
+  req.on('error', () => {
+    res.statusCode = 500;
+    res.end();
+  });
+  req.on('data', (data) => {
+    body += data;
+  });
+  req.on('end', () => {
+    fs.writeFile('filename', body, (err) =>{
+      if(err){
+        console.error(err);
+        res.statusCode = 500;
+        res.end();
+        return
+      }
 
-  switch (req.url) {
+      serveGallery(req, res);
+    });
+  });
+}
+
+var server = http.createServer((req, res) => {
+  //var url = req.url.split('?');
+  //var resource = url[0];
+  //var queryString = url[1];
+
+  var urlParts = url.parse(req.url);
+
+  if(urlParts.query){
+    var matches = /title=(.+)($|&)/.exec(urlParts.query);
+    if(matches && matches[1]){
+      config.title = decodeURIComponent(matches[1]);
+      fs.writeFile('config.json', JSON.stringify(config));
+    }
+  }
+
+  switch (urlParts.pathname) {
     case '/':
     case '/gallery':
       serveGallery(req, res);
       break;
     case '/gallery.css':
-      res.setHeader('Content-Type', 'text/css');
-      res.end(stylesheet);
+        if(req.method == "GET"){
+          res.setHeader('Content-Type', 'text/css');
+          res.end(stylesheet);
+        }
+        if(req.method == "POST"){
+          uploadImage(req, res);
+        }
+
       break;
     default:
-      serveImage(req.url, req, res);
+      serveImage(urlParts.pathname, req, res);
       break;
   }
 });
